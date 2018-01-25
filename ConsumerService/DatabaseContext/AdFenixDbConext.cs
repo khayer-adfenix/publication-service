4875ee18478e8f2ece6060dc42ad223028273d0b
khayer <khayer@adfenix.com>
2018-01-25 19:31:31 +0600

    added files

---
+++ ConsumerService/DatabaseContext/AdFenixDbConext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AdFenix.Entities.DomainObject;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsumerService.DatabaseContext
+{
+    public class AdFenixDbConext : DbContext
+    {
+        public DbSet<PublicationOwner> PublicationOwners { get; set; }
+
+        public AdFenixDbConext(DbContextOptions<AdFenixDbConext> options) : base(options)
+        {
+            
+            Database.Migrate();
+
+        }
+    }
+}
